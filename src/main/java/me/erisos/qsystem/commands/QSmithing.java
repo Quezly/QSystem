37de63b9e4feb7e04e9bdcb4edcff1b2b25689dd
Adem Mertkan Durmaz <ademmertkan01@gmail.com>
2023-12-25 04:52:21 +0300

    örs and zırhtasarım

--- src/main/java/me/erisos/qsystem/commands/QSmithing.java
+++ src/main/java/me/erisos/qsystem/commands/QSmithing.java
@@ -7,11 +7,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryType;
 
-public class QAnvil implements CommandExecutor {
+public class QSmithing implements CommandExecutor {
 
     private final QSystem plugin;
 
-    public QAnvil(QSystem plugin) {
+    public QSmithing(QSystem plugin) {
         this.plugin = plugin;
     }
 
@@ -19,7 +19,7 @@ public class QAnvil implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (!(commandSender instanceof Player player)) return true;
 
-        var inventory = plugin.getServer().createInventory(null, InventoryType.ANVIL, "Örs");
+        var inventory = plugin.getServer().createInventory(null, InventoryType.SMITHING, "Zırh Tasarımcısı");
         player.openInventory(inventory);
 
         return true;
