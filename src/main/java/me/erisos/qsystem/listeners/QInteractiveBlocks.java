40adc3d0a99fe4bdf387530c6154c99e82d49a2f
Adem Mertkan Durmaz <ademmertkan01@gmail.com>
2023-12-31 11:27:26 +0300

    pre2-1.2.5

--- src/main/java/me/erisos/qsystem/listeners/QInteractiveBlocks.java
+++ src/main/java/me/erisos/qsystem/listeners/QInteractiveBlocks.java
@@ -4,8 +4,10 @@ import me.despical.commons.util.Strings;
 import me.erisos.qsystem.QSystem;
 import org.bukkit.Material;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class QInteractiveBlocks implements Listener {
@@ -18,11 +20,13 @@ public class QInteractiveBlocks implements Listener {
 
     @EventHandler
     public void ClickBlock(PlayerInteractEvent e) {
-        if (plugin.getConfig().getBoolean("interactive_blocks.disable_crafting_table")) {
-            if (e.getAction().toString().contains("RIGHT_CLICK_BLOCK")) {
+        Player player = e.getPlayer();
+
+        if (plugin.getConfig().getBoolean("interactive_blocks.disable_blocks")) {
+            if (e.getAction() == Action.RIGHT_CLICK_BLOCK && e.getClickedBlock() != null && BlockList(e.getClickedBlock().getType())) {
                e.setCancelled(BlockList(e.getClickedBlock().getType()));
                if (plugin.getConfig().getBoolean("interactive_blocks.disable_blocks_send_message")) {
-                   Strings.format(plugin.getConfig().getString("interactive_blocks.disable_blocks_message"));
+                   player.sendMessage(Strings.format(plugin.getConfig().getString("interactive_blocks.disable_blocks_message")));
                }
             }
         }
